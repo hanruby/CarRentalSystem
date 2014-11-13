381912d8651df56f1972d0901fa750905551d16e
Kidsan <kieran_nothing@hotmail.com>
2014-11-13 23:51:01 +0000

    Create VehicleData.java

---
+++ VehicleData.java
@@ -0,0 +1,7 @@
+
+public abstract class VehicleData {
+	abstract public String[] carData(Vehicle v);
+	abstract public String[] busData(Vehicle v);
+	abstract public String[] bikeData(Vehicle v);
+
+}
