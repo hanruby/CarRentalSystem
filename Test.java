032532b3ceec86ce9719bb1bac99db3dae90f8ec
Kidsan <kieran_nothing@hotmail.com>
2014-11-13 23:54:12 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -10,6 +10,15 @@ public class Test {
 		makeCarRental m1 = new makeCarRental();
 		Rental c1 = m1.makeRental();
 		c1.saveRental();
+		Statement s = new BikeStatement();
+		s.generateStatement(c1);
+		Bus v1 = new Bus("Renault",50);
+		System.out.println(v1.getMaxCapacity());
+		System.out.println(v1.getCostPerDay());
+		Vehicle f = new Bus("Renault",30);
+		String [] sad = f.info();
+		System.out.print(sad[0]);
+		System.out.print(sad[1]);
 
 	}
 
