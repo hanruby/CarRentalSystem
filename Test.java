48e3ec7451902346784af3c6cd16bf38cf741948
Kidsan <kieran_nothing@hotmail.com>
2014-11-14 01:49:52 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -10,6 +10,20 @@ public class Test {
 		makeCarRental m1 = new makeCarRental();
 		Rental c1 = m1.makeRental();
 		c1.saveRental();
+		StatementWrapper b = new StatementWrapper();
+		StatementWrapper one = BasicStatement.getSingletonInstance();
+		StatementWrapper two = CarStatement.getSingletonInstance(one);
+		StatementWrapper three = BusStatement.getSingletonInstance(one);
+		StatementWrapper four = BikeStatement.getSingletonInstance(one);
+		two.generateStatement(c1);
+		Bus v1 = new Bus("Renault",50);
+		System.out.println(v1.getMaxCapacity());
+		System.out.println(v1.getCostPerDay());
+		Vehicle f = new Bus("Renault",30);
+		String [] sad = f.info();
+		System.out.print(sad[0]);
+		System.out.print(sad[1]);
+		
 
 	}
 
