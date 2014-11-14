f457d1cba557f955e0577d24016404d36881122c
Kidsan <kieran_nothing@hotmail.com>
2014-11-14 01:47:25 +0000

    Update StatementWrapper.java

--- StatementWrapper.java
+++ StatementWrapper.java
@@ -1,30 +1,50 @@
 
 public class StatementWrapper implements Statement {
 
-	@Override
+	private StatementWrapper successor;
+	
+	StatementWrapper getSuccessor()
+	{
+		return successor;
+	}
+	
+	void setSuccessor(StatementWrapper successor) {
+		this.successor = successor;
+		}
+	
+	protected Boolean canGenerate(Rental r,String test)
+	{
+		if(r.getType().equalsIgnoreCase(test))
+			return true;
+		else
+			return false;
+	}
+	
 	public void generateStatement(Rental r) {
 		System.out.println("Your Statement is being generated");
-		if(r.getType().equalsIgnoreCase("car"))
-		{
-			CarStatement c = new CarStatement();
-			c.generateStatement(r);
+		if ( getSuccessor() != null ){
+			if(r.getType().equalsIgnoreCase("car"))
+			{
+				CarStatement c = CarStatement.getSingletonInstance(this);
+				c.generateStatement(r);
+			}
+			if(r.getType().equalsIgnoreCase("bike"))
+			{
+				BikeStatement b = BikeStatement.getSingletonInstance(this);
+				b.generateStatement(r);
+			}
+			if(r.getType().equalsIgnoreCase("bus"))
+			{
+				BusStatement b = BusStatement.getSingletonInstance(this);
+				b.generateStatement(r);
+			}
+			else
+			{
+				BasicStatement b = BasicStatement.getSingletonInstance();
+				b.generateStatement(r);
+			}
 		}
-		if(r.getType().equalsIgnoreCase("bike"))
-		{
-			BikeStatement b = new BikeStatement();
-			b.generateStatement(r);
-		}
-		if(r.getType().equalsIgnoreCase("bus"))
-		{
-			BusStatement b = new BusStatement();
-			b.generateStatement(r);
-		}
-		else
-		{
-			BasicStatement b = new BasicStatement();
-			b.generateStatement(r);
-		}
-		
+
 	}
 
 }
