728d51fb3a3781d6ecff42bb1bc3c1f5aa338254
Kidsan <kieran_nothing@hotmail.com>
2014-11-14 01:48:31 +0000

    Update BusStatement.java

--- BusStatement.java
+++ BusStatement.java
@@ -3,26 +3,44 @@ import java.io.FileWriter;
 
 public class BusStatement extends StatementWrapper
 {
+	private static final BusStatement singletonInstance = new BusStatement();
+	private static StatementWrapper successor;
+	
+	private BusStatement()
+	{
+		this.setSuccessor(successor);
+	}
+	
+	public static BusStatement getSingletonInstance(StatementWrapper s)
+	{
+		successor = s;
+		return singletonInstance;
+	}
+	
 	@Override
 	public void generateStatement(Rental r)
 	{
-		
-		try
+		if(canGenerate(r,"bus"))
 		{
-			FileWriter writer = new FileWriter(r.getOwner() + "Statement.txt",false);
-			writer.append("This is your Bus Rental Statement"+System.lineSeparator());
-			writer.append(r.getOwner()+"\'s Bus Rental" + System.lineSeparator());
-			writer.append("Vehicle type: " + r.getType()+System.lineSeparator());
-			writer.append("Rental Duration: " + r.getDaysRequested() + " days" + System.lineSeparator());
-			writer.append("Cost Per Day: " + r.getCostPerDay() + System.lineSeparator());
-			writer.append("Total Paid: " + (r.getCostPerDay()*r.getDaysRequested()) + System.lineSeparator());
-			writer.append("We hope you enjoyed driving our " + r.getModel() +" Bus!"+ System.lineSeparator());
-			writer.close();
+			try
+			{
+				FileWriter writer = new FileWriter(r.getOwner() + "Statement.txt",false);
+				writer.append("This is your Bus Rental Statement"+System.lineSeparator());
+				writer.append(r.getOwner()+"\'s Bus Rental" + System.lineSeparator());
+				writer.append("Vehicle type: " + r.getType()+System.lineSeparator());
+				writer.append("Rental Duration: " + r.getDaysRequested() + " days" + System.lineSeparator());
+				writer.append("Cost Per Day: " + r.getCostPerDay() + System.lineSeparator());
+				writer.append("Total Paid: " + (r.getCostPerDay()*r.getDaysRequested()) + System.lineSeparator());
+				writer.append("We hope you enjoyed driving our " + r.getModel() +" Bus!"+ System.lineSeparator());
+				writer.close();
+			}
+			catch(Exception e)
+			{
+				System.out.println("Failed to make Bus Statement");
+			}
 		}
-		catch(Exception e)
-		{
-			System.out.println("Failed to make Bus Statement");
+		else{
+			super.generateStatement(r);
 		}
-		
 	}
 }
